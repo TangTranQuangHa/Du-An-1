9eee4d2ccfbaae0156b1b156b584e4284426b439
Tăng Trần Quang Hạ <tangtranquangha@gmail.com>
2026-08-14 02:08:17 +0700

    Ha SaveData

--- Assets/Scripts/Utilities/SceneUtilities.cs
+++ Assets/Scripts/Utilities/SceneUtilities.cs
@@ -5,10 +5,6 @@ public class SceneUtilities : MonoBehaviour
 {
     public void GoToMainMenu()
     {
-        var gm = GameManager.Instance;
-        gm.ResetGameValues();
-        gm.heroManager.GiveStarterHeroes();
-        gm.Save();
         SceneManager.LoadScene("MainMenu");
     }
 }
